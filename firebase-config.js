c054970d8fcf9df5f0cc412df793c35ce6d9a4b2
Virajit Mungale <43114008+vmungale@users.noreply.github.com>
2026-06-23 08:22:31 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "uktoindia-book.firebaseapp.com",
+  projectId: "uktoindia-book",
+  storageBucket: "uktoindia-book.firebasestorage.app",
+  messagingSenderId: "317688052141",
+  appId: "1:317688052141:web:a742972b766b03c6a38924"
+};
